fe32713331f093be34219fa4049726087ecdd319
sezin akbulut <sezin.akbulut@gmail.com>
2023-12-18 12:15:08 +0100

    database is connected

---
+++ client/client.js
@@ -0,0 +1,29 @@
+server.get("/show-all", async (req, res) => {
+  // database connection
+  // execute query
+  // send response with data
+})(
+  //GET
+  async () => {
+    const response = await fetch("http://localhost:3000/show-ideas");
+    const data = await response.json();
+    console.log(data);
+  }
+)();
+
+//POST
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    name: userName, // variable with the value you want to send
+  }),
+};
+
+(async () => {
+  const response = await fetch("http://localhost:3000/new-idea", options);
+  const data = await response.json();
+  console.log(data);
+})();
--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
-import mariadb from "mariadb";
-import * as dotenv from "dotenv";
+const mariadb = require("mariadb");
+const dotenv = require("dotenv");
 dotenv.config();
 
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/ideas/:id", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(80, function () {
+  console.log("CORS-enabled web server listening on port 80");
+});
+
+//POOL
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -9,17 +25,34 @@ const pool = mariadb.createPool({
   connectionLimit: 5,
 });
 
+//EXPRESS
+
+// the wrapper around the connection & try-catch-finally is just nameless arrow function to create an asynchronous environment.
 (async () => {
   let connection;
   try {
     connection = await pool.getConnection();
+    console.log("database is connected");
+
     const data = await connection.query(`SELECT * FROM brilliant_minds.ideas`);
     console.log(data);
   } catch (err) {
+    console.log(err);
+
     throw err;
   } finally {
     if (connection) connection.end();
   }
 })();
 
-// the wrapper around the connection & try-catch-finally is just nameless arrow function to create an asynchronous environment.
+//PORT
+const port = process.env.PORT;
+app.listen(port, () =>
+  console.log(`Server started: http://localhost:${port}/`)
+);
+
+app.get("/api", (req, res) => {
+  res.json({
+    text: "my api!",
+  });
+});
